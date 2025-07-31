f529c32bad3f1e8ab771a4df071781494cf4bd7d
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-07-31 12:51:46 +0545

    Initialized Django and created api for user and profile

--- django-rest-training/users/router.py
+++ django-rest-training/users/router.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
-from .viewsets import ProfileViewSet, UserViewSet
+from .viewsets import *
 
 app_name = "users"
+
 router = routers.DefaultRouter()
-router.register('user', UserViewSet)
-router.register('profile', ProfileViewSet)
+router.register('user', UserViewSet)        # Handles /api/users/user/
+router.register('profile', ProfileViewSet)  # Handles /api/users/profile/
