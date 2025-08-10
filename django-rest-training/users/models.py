afeeb6c572dc1300aecccae062d4b0bebe8e9937
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-08-10 22:02:40 +0545

    worked on projects

--- django-rest-training/users/models.py
+++ django-rest-training/users/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 def generateImagePath( instance, file):
-    return f'images/users/{instance.username}/{file}'
+    return f'images/users/{instance.user.username}/{file}'
 class Profile(models.Model):
     class RoleOptions(models.TextChoices):
         Employer= 'E', 'Employer'
         Worker= 'W', 'Worker'
         
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(choices=RoleOptions, default=RoleOptions.Worker, max_length=1)
+    role = models.CharField(choices=RoleOptions.choices, default=RoleOptions.Worker, max_length=1)
     address = models.CharField(blank=True, null=True, max_length=50)
     dob = models.DateField(blank=True, null=True)
     profile_image = models.ImageField(blank=True, null=True, upload_to=generateImagePath, default='images/users/default_user.jpg')
