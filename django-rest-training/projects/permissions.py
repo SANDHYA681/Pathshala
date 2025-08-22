6c92eee405a7f35dfbbfa912f95c076e682cb8c2
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-08-22 21:25:20 +0545

    worked on tasks app

--- django-rest-training/projects/permissions.py
+++ django-rest-training/projects/permissions.py
@@ -6,7 +6,7 @@ class IsProjectOwnerOrReadOnly(permissions.BasePermission):
     
     def has_object_permission(self, request, view, obj):
         
-        #gives permission to everyone to read the data in safe method
+        #gives permission to everyone to read the data in safe methods
         if request.method in permissions.SAFE_METHODS:
             return True
         
